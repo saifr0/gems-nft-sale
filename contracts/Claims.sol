ce8155a37150198449ad99eabc7539e9cd27be01
saifr0 <saifurehman980@gmail.com>
2025-02-10 15:27:48 +0500

    latest changes

--- contracts/Claims.sol
+++ contracts/Claims.sol
@@ -206,7 +206,7 @@ contract Claims is IClaims, AccessControl, ReentrancyGuardTransient {
                 IERC20 currentToken = token[i];
                 uint256 amount = claims[currentToken];
 
-                if (amount > 0) {
+                if (amount == 0) {
                     continue;
                 }
 
